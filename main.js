1ebb7b45f53f8e66d4f863ef880759a2b97980af
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-18 17:26:58 +0100

    Instantiate an object from the class

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 function Person(firstName, lastName, dob) {
+    // Set object properties
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBrithYear = function() {
-        return this.dob.getFullYear();
-    }
-    this.getFullName = function() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
+  }
+
+// Get Birth Year
+Person.prototype.getBirthYear = function () {
+    return this.dob.getFullYear();
+  }
+ // Get Full Name
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`
+  } 
+
+
+// Instantiate an object from the class
+const person1 = new Person('John', 'Doe', '7-8-80');
+const person2 = new Person('Steve', 'Smith', '8-2-90');
+
+console.log(person2.getFullName());
+console.log(person1);
